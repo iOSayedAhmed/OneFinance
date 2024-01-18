3c1c68f3cc28d7be531ace5a0f8b860dcd30d6d7
iOSayedAhmed <60572628+iOSayedAhmed@users.noreply.github.com>
2024-01-18 02:49:24 +0200

    add APIURLs & URLPaths 🤗

---
+++ OneFinanceTask/OneFinanceTask/Helpers/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  OneFinanceTask
+//
+//  Created by iOSAYed on 17/01/2024.
+//
+
+import Foundation
+
+enum APIURLs {
+    static let baseUrl = "https://fakestoreapi.com"
+}
+
+enum URLPaths {
+    static let login = "/auth/login"
+    static let allProducts = "/products"
+    static let productsDetails = "/products/"
+    static let addProduct = "/products"
+    static let allCategories = "/products/categories"
+}
