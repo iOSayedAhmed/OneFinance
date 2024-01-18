4b9c994565081382b89c3cb536fd5bb7f448b5dc
iOSayedAhmed <60572628+iOSayedAhmed@users.noreply.github.com>
2024-01-18 02:49:54 +0200

    Create BasicNetworkService 👽
    JUST APPLY DEFAULT IMPLEMENATION

---
+++ OneFinanceTask/OneFinanceTask/Helpers/BasicNetworkService.swift
@@ -0,0 +1,14 @@
+//
+//  BasicNetworkService.swift
+//  OneFinanceTask
+//
+//  Created by iOSAYed on 17/01/2024.
+//
+
+import Foundation
+import Alamofire
+
+final class BasicNetworkService:NetworkService {
+    // 1 .JUST APPLY DEFAULT IMPLEMENATION
+
+}
