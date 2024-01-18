cc2c3c4409c5988a7d5a0b822e4dc32b1ff05ad6
iOSayedAhmed <60572628+iOSayedAhmed@users.noreply.github.com>
2024-01-18 02:48:52 +0200

    Update SignupViewModel.swift

--- OneFinanceTask/OneFinanceTask/Modules/AuthModule/Signup/ViewModel/SignupViewModel.swift
+++ OneFinanceTask/OneFinanceTask/Modules/AuthModule/Signup/ViewModel/SignupViewModel.swift
@@ -11,7 +11,6 @@ import RxSwift
 import RxRelay
 
 protocol SignupViewModelProtocol {
-    func register(name:String,email: String,phone:String, password: String, deviceToken: String)
     func didTappedLogin()
     func didUserRegistered(userData:UserData)
 }
@@ -23,39 +22,10 @@ final class SignupViewModel: SignupViewModelProtocol {
     private var networkService:NetworkService?
     private let disposeBag = DisposeBag()
 
-    private let registerResultSubject = PublishSubject<LoginModel>()
-    
-    let isLoading = BehaviorRelay<Bool>(value: false)
-    
-    var registerResult: Observable<LoginModel> {
-           return registerResultSubject.asObservable()
-       }
     
     init(coordinator: SignupCoordinator? = nil, networkService: NetworkService? = nil) {
         self.coordinator = coordinator
         self.networkService = networkService
-    }
-    
-    
-    func register(name:String,email: String,phone:String, password: String, deviceToken: String) {
-        isLoading.accept(true)
-        guard let networkService = networkService else {
-               print("NetworkService is nil")
-               return
-           }
-        let params = ["name":name ,"email":email,"phone":phone,"password":password,"device_token":deviceToken]
-        
-        networkService.request(Endpoints.register(Parameters: params))
-            
-            .subscribe(onSuccess: { [weak self] (registerModel:LoginModel) in
-                guard let self  else {return}
-                self.isLoading.accept(false)
-                self.registerResultSubject.onNext(registerModel)
-            },onFailure: { error in
-                self.isLoading.accept(false)
-                print(error)
-            })
-            .disposed(by: disposeBag)
     }
     
     func didUserRegistered(userData:UserData){
